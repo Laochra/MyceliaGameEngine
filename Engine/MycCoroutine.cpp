d96fb160917cd68dd2a58f6d0c8fffb528847d22
Laochra <blue.cat.blue@outlook.com>
2024-10-20 12:25:53 +1100

    Fixed Issue that Could Arise From Recursive Coroutines Breaking Iterators

--- Engine/MycCoroutine.cpp
+++ Engine/MycCoroutine.cpp
@@ -2,10 +2,10 @@
 
 void Coroutine::Update(float delta) noexcept
 {
-	for (auto it = Coroutine::active.begin(); it != Coroutine::active.end();)
+	for (int i = 0; i < (int)Coroutine::active.size();)
 	{
-		Coroutine::FunctionInterface& coroutine = *it->first;
-		Coroutine::Package& package = it->second;
+		Coroutine::FunctionInterface& coroutine = *Coroutine::active[i].first;
+		Coroutine::Package& package = Coroutine::active[i].second;
 
 		if (package.yieldTimer > 0.0f)
 		{
@@ -17,12 +17,12 @@ void Coroutine::Update(float delta) noexcept
 			coroutine(package);
 			if (package.state == Coroutine::State::Finalised)
 			{
-				delete &coroutine;
-				it = Coroutine::active.erase(it);
+				delete& coroutine;
+				Coroutine::active.erase(Coroutine::active.begin() + i);
 				continue;
 			}
 		}
 
-		++it;
+		++i;
 	}
 }
